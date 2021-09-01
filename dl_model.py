e768a9fc43ac38d38e0f58445a967749428e86d3
Matthew Lowery <mattlowery@Matthews-MacBook-Pro.local>
2021-09-01 16:20:39 -0400

    added optimization model

--- dl_model.py
+++ dl_model.py
@@ -1,5 +1,3 @@
-
-
 import time
 import numpy as np
 import h5py
@@ -70,12 +68,10 @@ def initialize_parameters_deep(layer_dims):
 def linear_forward(A, W, b):
     """
     Implement the linear part of a layer's forward propagation.
-
     Arguments:
     A -- activations from previous layer (or input data): (size of previous layer, number of examples)
     W -- weights matrix: numpy array of shape (size of current layer, size of previous layer)
     b -- bias vector, numpy array of shape (size of the current layer, 1)
-
     Returns:
     Z -- the input of the activation function, also called pre-activation parameter 
     cache -- a python tuple containing "A", "W" and "b" ; stored for computing the backward pass efficiently
@@ -88,87 +84,15 @@ def linear_forward(A, W, b):
     return Z, cache
 
 
-def compute_cost_with_regularization(A3, Y, parameters, lambd):
-    """
-    Implement the cost function with L2 regularization. 
-    
-    Arguments:
-    A3 -- post-activation, output of forward propagation, of shape (output size, number of examples)
-    Y -- "true" labels vector, of shape (output size, number of examples)
-    parameters -- python dictionary containing parameters of the model
-    
-    Returns:
-    cost - value of the regularized loss function (formula (2))
-    """
-    m = Y.shape[1]
-    W1 = parameters["W1"]
-    W2 = parameters["W2"]
-    W3 = parameters["W3"]
-    
-    cross_entropy_cost = compute_cost(A3, Y) # This gives you the cross-entropy part of the cost
-    
-    L2_regularization_cost = (np.sum(np.square(W1)) + np.sum(np.square(W2)) + np.sum(np.square(W3))) * (1/m) * (lambd/2)
-    
-    cost = cross_entropy_cost + L2_regularization_cost
-    
-    return cost
-
-def backward_propagation_with_regularization(X, Y, cache, lambd):
-    """
-    Implements the backward propagation of our baseline model to which we added an L2 regularization.
-    
-    Arguments:
-    X -- input dataset, of shape (input size, number of examples)
-    Y -- "true" labels vector, of shape (output size, number of examples)
-    cache -- cache output from forward_propagation()
-    lambd -- regularization hyperparameter, scalar
-    
-    Returns:
-    gradients -- A dictionary with the gradients with respect to each parameter, activation and pre-activation variables
-    """
-    
-    m = X.shape[1]
-    (Z1, A1, W1, b1, Z2, A2, W2, b2, Z3, A3, W3, b3) = cache
-    
-    dZ3 = A3 - Y
-
-    dW3 = 1./m * np.dot(dZ3, A2.T) + (lambd / m)*W3
-    
-    db3 = 1. / m * np.sum(dZ3, axis=1, keepdims=True)
-    
-    dA2 = np.dot(W3.T, dZ3)
-    dZ2 = np.multiply(dA2, np.int64(A2 > 0))
-
-    dW2 = 1./m * np.dot(dZ2, A1.T) + (lambd / m)*W2
-    
-    db2 = 1. / m * np.sum(dZ2, axis=1, keepdims=True)
-    
-    dA1 = np.dot(W2.T, dZ2)
-    dZ1 = np.multiply(dA1, np.int64(A1 > 0))
-
-    dW1 = 1./m * np.dot(dZ1, X.T)  + (lambd / m)*W1
-    
- 
-    db1 = 1. / m * np.sum(dZ1, axis=1, keepdims=True)
-    
-    gradients = {"dZ3": dZ3, "dW3": dW3, "db3": db3,"dA2": dA2,
-                 "dZ2": dZ2, "dW2": dW2, "db2": db2, "dA1": dA1, 
-                 "dZ1": dZ1, "dW1": dW1, "db1": db1}
-    
-    return gradients
-
-
 
 def linear_activation_forward(A_prev, W, b, activation):
     """
     Implement the forward propagation for the LINEAR->ACTIVATION layer
-
     Arguments:
     A_prev -- activations from previous layer (or input data): (size of previous layer, number of examples)
     W -- weights matrix: numpy array of shape (size of current layer, size of previous layer)
     b -- bias vector, numpy array of shape (size of the current layer, 1)
     activation -- the activation to be used in this layer, stored as a text string: "sigmoid" or "relu"
-
     Returns:
     A -- the output of the activation function, also called the post-activation value 
     cache -- a python tuple containing "linear_cache" and "activation_cache";
@@ -190,8 +114,6 @@ def linear_activation_forward(A_prev, W, b, activation):
     cache = (linear_cache, activation_cache)
 
     return A, cache
-
-
 
 
 
@@ -236,11 +158,9 @@ def L_model_forward(X, parameters):
 def compute_cost(AL, Y):
     """
     Implement the cost function defined by equation (7).
-
     Arguments:
     AL -- probability vector corresponding to your label predictions, shape (1, number of examples)
     Y -- true "label" vector (for example: containing 0 if non-cat, 1 if cat), shape (1, number of examples)
-
     Returns:
     cost -- cross-entropy cost
     """
@@ -262,11 +182,9 @@ def compute_cost(AL, Y):
 def linear_backward(dZ, cache):
     """
     Implement the linear portion of backward propagation for a single layer (layer l)
-
     Arguments:
     dZ -- Gradient of the cost with respect to the linear output (of current layer l)
     cache -- tuple of values (A_prev, W, b) coming from the forward propagation in the current layer
-
     Returns:
     dA_prev -- Gradient of the cost with respect to the activation (of the previous layer l-1), same shape as A_prev
     dW -- Gradient of the cost with respect to W (current layer l), same shape as W
@@ -402,100 +320,12 @@ def update_parameters(params, grads, learning_rate):
 
 
 
-def two_layer_model(X, Y, layers_dims, learning_rate = 0.0075, num_iterations = 3000, print_cost=False):
-    """
-    Implements a two-layer neural network: LINEAR->RELU->LINEAR->SIGMOID.
-    
-    Arguments:
-    X -- input data, of shape (n_x, number of examples)
-    Y -- true "label" vector (containing 1 if cat, 0 if non-cat), of shape (1, number of examples)
-    layers_dims -- dimensions of the layers (n_x, n_h, n_y)
-    num_iterations -- number of iterations of the optimization loop
-    learning_rate -- learning rate of the gradient descent update rule
-    print_cost -- If set to True, this will print the cost every 100 iterations 
-    
-    Returns:
-    parameters -- a dictionary containing W1, W2, b1, and b2
-    """
-    
-    
-    grads = {}
-    costs = []                              # to keep track of the cost
-    m = X.shape[1]                           # number of examples
-    (n_x, n_h, n_y) = layers_dims
-    
-    # Initialize parameters dictionary, by calling previously implemented function
-
-    
-    parameters = initialize_parameters(n_x, n_h, n_y)
-    
-    
-    # Get W1, b1, W2 and b2 from the dictionary parameters.
-    W1 = parameters["W1"]
-    b1 = parameters["b1"]
-    W2 = parameters["W2"]
-    b2 = parameters["b2"]
-    
-    # Loop (gradient descent)
-
-    for i in range(0, num_iterations):
-
-        # Forward propagation: LINEAR -> RELU -> LINEAR -> SIGMOID. Inputs: "X, W1, b1, W2, b2". Output: "A1, cache1, A2, cache2".
-
-        A1, cache1 = linear_activation_forward(X, W1, b1, 'relu')
-        A2, cache2 = linear_activation_forward(A1, W2, b2, 'sigmoid')
-        
-  
-        # Compute cost
-
-        cost = compute_cost(A2, Y)
-        
-
-        # Initializing backward propagation
-        dA2 = - (np.divide(Y, A2) - np.divide(1 - Y, 1 - A2))
-        
-        # Backward propagation. Inputs: "dA2, cache2, cache1". Outputs: "dA1, dW2, db2; also dA0 (not used), dW1, db1".
-
-        dA1, dW2, db2 = linear_activation_backward(dA2, cache2, 'sigmoid')
-        dA0, dW1, db1 = linear_activation_backward(dA1, cache1, 'relu')
-        
-        
-        
-        # Set grads['dWl'] to dW1, grads['db1'] to db1, grads['dW2'] to dW2, grads['db2'] to db2
-        grads['dW1'] = dW1
-        grads['db1'] = db1
-        grads['dW2'] = dW2
-        grads['db2'] = db2
-        
-        # Update parameters.
-
-        parameters = update_parameters(parameters, grads, learning_rate)
-        
- 
-
-        # Retrieve W1, b1, W2, b2 from parameters
-        W1 = parameters["W1"]
-        b1 = parameters["b1"]
-        W2 = parameters["W2"]
-        b2 = parameters["b2"]
-        
-        # Print the cost every 100 iterations
-        if print_cost and i % 100 == 0 or i == num_iterations - 1:
-            print("Cost after iteration {}: {}".format(i, np.squeeze(cost)))
-        if i % 100 == 0 or i == num_iterations:
-            costs.append(cost)
-
-    return parameters, costs
-
 def plot_costs(costs, learning_rate=0.0075):
     plt.plot(np.squeeze(costs))
     plt.ylabel('cost')
     plt.xlabel('iterations (per hundreds)')
     plt.title("Learning rate =" + str(learning_rate))
     plt.show()
-
-
-
 
 
 def L_layer_model(X, Y, layers_dims, learning_rate = 0.0075, num_iterations = 3000, print_cost=False):
@@ -550,7 +380,6 @@ def L_layer_model(X, Y, layers_dims, learning_rate = 0.0075, num_iterations = 30
     return parameters, costs
 
 
-
 def predict(X, y, parameters):
     """
     This function is used to predict the results of a  L-layer neural network.
@@ -585,11 +414,3 @@ def predict(X, y, parameters):
     print("Accuracy: "  + str(np.sum((p == y)/m)))
         
     return p
-
-
-
-
-
-
-
-
